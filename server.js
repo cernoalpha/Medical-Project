077ce106eca42588bf7a4948abe0d9bf8a51145e
cernoalpha <mirshahzadmubeen1@gmail.com>
2024-01-14 14:42:05 +0530

    new new

--- server.js
+++ server.js
@@ -10,7 +10,13 @@ const PORT = process.env.PORT || 5001;
 
 
 app.use(bodyParser.json({ limit: '10mb' }));
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 const upload = multer();
 
